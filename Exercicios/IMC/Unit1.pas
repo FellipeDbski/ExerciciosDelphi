a59643c54de4f343bae987a9d5d1fe437e9697ab
Fellipe Dobokowski <dobokowskifellipe@gmail.com>
2018-08-26 00:42:25 -0300

    Exercicios 5 e 6 completos

---
+++ Exercicios/IMC/Unit1.pas
@@ -0,0 +1,43 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    edPeso: TEdit;
+    lbPeso: TLabel;
+    edAltura: TEdit;
+    lbAltura: TLabel;
+    btCalcula: TButton;
+    edIMC: TEdit;
+    lbIMC: TLabel;
+    procedure btCalculaClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btCalculaClick(Sender: TObject);
+var
+   wPeso, wAltura, wIMC : Real;
+begin
+   wPeso := strToFloat(edPeso.Text);
+   wAltura := strToFloat(edAltura.Text);
+   wIMC := wPeso / sqr(wAltura);
+
+   edIMC.Text := formatfloat('###,###,##0.00', wIMC);
+end;
+
+end.
